dc6089954174e60275e34a4db2175c442d3918df
nygmasx <imraneeslk@gmail.com>
2024-03-13 09:56:51 +0100

    added assets vendor

---
+++ assets/vendor/installed.php
@@ -0,0 +1,106 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/fade-theme' => 
+  array (
+    'version' => '1.0.5',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/slide-theme' => 
+  array (
+    'version' => '1.0.5',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/forms-plugin' => 
+  array (
+    'version' => '2.0.1',
+    'dependencies' => 
+    array (
+      0 => '@swup/plugin',
+      1 => 'swup',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/plugin' => 
+  array (
+    'version' => '2.0.3',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'swup' => 
+  array (
+    'version' => '3.1.1',
+    'dependencies' => 
+    array (
+      0 => 'delegate-it',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'delegate-it' => 
+  array (
+    'version' => '6.0.1',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/debug-plugin' => 
+  array (
+    'version' => '3.0.0',
+    'dependencies' => 
+    array (
+      0 => '@swup/plugin',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@domchristie/turn' => 
+  array (
+    'version' => '3.1.1',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
