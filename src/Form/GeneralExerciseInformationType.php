64059fc5d30ec5b20463bec46621278515ef5895
nygmasx <imraneeslk@gmail.com>
2024-03-18 08:46:55 +0100

    Working on

--- src/Form/GeneralExerciseInformationType.php
+++ src/Form/GeneralExerciseInformationType.php
@@ -24,35 +24,27 @@ class GeneralExerciseInformationType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, [
-                "label" => "Nom de l'exercice",
-                'required' => true
-            ])
-            ->add('course', EntityType::class, [
-                "label" => "Matière",
-                'class' => Course::class,
-                'choice_label' => 'name',
-            ])
-            ->add('classroom', EntityType::class, [
-                'class' => Classroom::class,
-                'label' => 'Classe',
-                'choice_label' => 'name',
-            ])
-            ->add('thematic', EntityType::class, [
-                'class' => Thematic::class,
-                'label' => 'Thématique',
-                'choice_label' => 'name',
+                'label' => 'Nom'
             ])
             ->add('chapter', TextType::class, [
                 'label' => 'Chapitre'
             ])
-            ->add('keywords', TextType::class, [
+            ->add('keywords', ChoiceTagType::class, [
                 'label' => 'Mots Clés'
             ])
-            ->add('difficulty', ChoiceType::class, [
-                'label' => 'Difficulté'
+            ->add('difficulty')
+            ->add('duration')
+            ->add('course', EntityType::class, [
+                'class' => Course::class,
+                'choice_label' => 'id',
             ])
-            ->add('duration', TextType::class, [
-                'label' => 'Durée (en heure)'
+            ->add('classroom', EntityType::class, [
+                'class' => Classroom::class,
+                'choice_label' => 'id',
+            ])
+            ->add('thematic', EntityType::class, [
+                'class' => Thematic::class,
+                'choice_label' => 'id',
             ])
             ->add('skill', EntityType::class, [
                 'class' => Skill::class,
