9176a5c7cf80bc3b79d81d4e3d862d533e1ed5dd
nygmasx <imraneeslk@gmail.com>
2024-04-12 11:44:27 +0200

    adding feat-2

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,43 +2,41 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\UserType;
-use App\Security\LoginAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use App\Security\LoginAuthenticator;
+
 
 class UserController extends AbstractController
 {
-    public function __construct(
-        private UserPasswordHasherInterface $passwordHasher,
-        private UserAuthenticatorInterface $userAuthenticator,
-        private LoginAuthenticator $loginAuthenticator,
-        private EntityManagerInterface $entityManager,
-    ) {}
 
-    #[Route('/add/user', name: 'add_user')]
-    public function addUser(Request $request, AuthorizationCheckerInterface $authChecker): Response
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher,
+    private readonly UserAuthenticatorInterface $userAuthenticator,
+    private readonly LoginAuthenticator $loginAuthenticator,
+    private readonly EntityManagerInterface $entityManager,)
     {
 
-        // Vérifie si l'utilisateur a le rôle admin
-        if (!$authChecker->isGranted('ROLE_ADMIN')) {
-            throw $this->createAccessDeniedException('Accès refusé. Vous n\'avez pas les permissions nécessaires.');
-        }
+    }
 
+    #[Route('/add/user', name: 'add_user')]
+    public function addUser(Request $request): Response
+    {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
+
         if ($form->isSubmitted() && $form->isValid()) {
-            // Encode le mot de passe
+            // Encode the plain password
             $user->setPassword(
                 $this->passwordHasher->hashPassword(
                     $user,
@@ -46,73 +44,69 @@ class UserController extends AbstractController
                 )
             );
 
-            // Récupère les rôles depuis le formulaire
-            $role = $form->get('role')->getData();
-            $roles = $user->getRoles();
-            // Assure que les rôles sont un tableau
-            if (empty($roles) || !is_array($roles)) {
-                $roles = [$role];
-            }
-            // Assure que les rôles sont uniques
-            $roles = array_unique($roles);
-            
-            // Attribue les rôles à l'utilisateur
-            $user->setRoles($roles);
-
             $this->entityManager->persist($user);
             $this->entityManager->flush();
 
             return $this->redirectToRoute('app_contributors');
+            // Vous voudrez peut-être ajuster la route 'app_contributors' selon vos besoins
         }
 
-        return $this->render('admin/contributors/add.html.twig', [
+        return $this->render('admin/add.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/user-delete/{id}', name: 'user_delete', methods: ['GET', 'POST'])]
-    public function delete(Request $request, User $user, AuthorizationCheckerInterface $authChecker): Response
+    private function createDeleteConfirmationForm(): \Symfony\Component\Form\FormInterface
     {
-
-        // Vérifie si l'utilisateur a le rôle admin
-        if (!$authChecker->isGranted('ROLE_ADMIN')) {
-            throw $this->createAccessDeniedException('Accès refusé. Vous n\'avez pas les permissions nécessaires.');
-        }
-
-        $form = $this->createDeleteConfirmationForm();
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->remove($user);
-            $this->entityManager->flush();
-
-            $this->addFlash('success', 'L\'utilisateur a été supprimé avec succès.');
-
-            return $this->redirectToRoute('app_contributors');
-        }
-
-        return $this->render('admin/contributors/delete.html.twig', [
-            'user' => $user,
-            'confirmationForm' => $form->createView(),
-        ]);
+        return $this->createFormBuilder()
+            ->add('confirm', SubmitType::class, ['label' => 'Confirmer la suppression'])
+            ->getForm();
     }
 
-    #[Route('/edit/user/{id}', name: 'edit_user')]
-    public function editUser(User $user, Request $request, AuthorizationCheckerInterface $authChecker): Response
-    {
+    #[Route('/user-delete/{id}', name: 'user_delete', methods: ['GET', 'POST'])]
+public function delete(Request $request, string $id): Response
+{
+    // Utilisation de findBy au lieu de find
+    $user = $this->entityManager->getRepository(User::class)->findBy(['id' => $id]);
 
-        // Vérifie si l'utilisateur a le rôle admin
-        if (!$authChecker->isGranted('ROLE_ADMIN')) {
-            throw $this->createAccessDeniedException('Accès refusé. Vous n\'avez pas les permissions nécessaires.');
-        }
-        
+    // findBy retourne un tableau, donc vérifiez si le tableau est vide
+    if (empty($user)) {
+        throw $this->createNotFoundException('L\'user n\'a pas été trouvé.');
+    }
+
+    // Comme findBy retourne un tableau, prenez le premier élément
+    $user = $user[0];
+
+    $form = $this->createDeleteConfirmationForm();
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'L\'user a été supprimé avec succès.');
+
+        return $this->redirectToRoute('app_contributors');
+    }
+
+    // Afficher la vue de confirmation si le formulaire n'a pas été soumis/validé
+    return $this->render('admin/delete.html.twig', [
+        'user' => $user,
+        'confirmationForm' => $form->createView(),
+    ]);
+}
+
+
+    #[Route('/edit/user/{id}', name: 'edit_user')]
+    public function editUser(User $user, Request $request): Response
+    {
         $form = $this->createForm(UserType::class, $user, [
             'require_password' => false,
         ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $plainPassword = $form->get('password')->getData();
+            $plainPassword = $form->get('plainPassword')->getData();
             if (!empty($plainPassword)) {
                 $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
                 $user->setPassword($hashedPassword);
@@ -128,12 +122,5 @@ class UserController extends AbstractController
         return $this->render('admin/contributors/edit.html.twig', [
             'form' => $form->createView(),
         ]);
-    }
-
-    private function createDeleteConfirmationForm(): \Symfony\Component\Form\FormInterface
-    {
-        return $this->createFormBuilder()
-            ->add('confirm', SubmitType::class, ['label' => 'Confirmer la suppression'])
-            ->getForm();
     }
 }
