1aadab2c8c04f8a688ac5d7ac95571cd2ab2be9d
Guerda Yacine <124626275+BouderBaden@users.noreply.github.com>
2024-04-22 10:13:17 +0200

    test

--- src/DataFixtures/ClassroomFixtures.php
+++ src/DataFixtures/ClassroomFixtures.php
@@ -8,31 +8,32 @@ use App\Entity\Classroom;
 
 class ClassroomFixtures extends Fixture
 {
+
+    public const REFERENCE_IDENTIFIER = 'classroom_';
+
+    public const CLASSROOM = [
+        '6ème',
+        '5ème',
+        '4ème',
+        '3ème',
+        'Seconde',
+        'Première',
+        'Terminale',
+        'BTS-01',
+        'BTS-02',
+        'Licence',
+    ];
+
+
     public function load(ObjectManager $manager): void
     {
-        // Créer des classes fictives
-        $classesData = [
-            'Collège' => ['6ème', '5ème', '4ème', '3ème'],
-            'Lycée' => ['Seconde', 'Première', 'Terminale'],
-            'BTS' => ['BTS01', 'BTS02'],
-            'Autre' => ['Licence'],
-        ];
+        foreach (self::CLASSROOM as $i => $classroomData) {
+            $classroom = (new Classroom())
+                ->setName(self::CLASSROOM[$i]);
 
-        foreach ($classesData as $group => $classes) {
-            foreach ($classes as $className) {
-                $classroom = new Classroom();
-                $classroom->setName($className);
-
-                // Persiste l'entité
-                $manager->persist($classroom);
-
-                // Ajoute une référence
-                $referenceName = 'classroom_' . strtolower(str_replace(' ', '_', $className));
-                $this->addReference($referenceName, $classroom);
-            }
+            $manager->persist($classroom);
+            $this->addReference(self::REFERENCE_IDENTIFIER . $i, $classroom);
         }
-
-        // Flush toutes les opérations en base de données
         $manager->flush();
     }
 }
